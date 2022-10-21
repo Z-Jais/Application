9b68814043ca762a9fa52f385993721eb3279246
Ziedelth <watteaupierre@gmail.com>
2022-10-21 23:41:32 +0200

    add review request

--- lib/mappers/device_mapper.dart
+++ lib/mappers/device_mapper.dart
@@ -2,11 +2,15 @@ import 'dart:convert';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_review/in_app_review.dart';
 import 'package:jais/url/url.dart';
 import 'package:jais/url/url_const.dart';
 import 'package:platform_device_id_v3/platform_device_id.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DeviceMapper {
+  static final ReviewMapper reviewMapper = ReviewMapper();
+
   static bool isOnMobile(BuildContext context, [double width = 600]) {
     return MediaQuery.of(context).size.width < width;
   }
@@ -19,14 +23,14 @@ class DeviceMapper {
   @pragma('vm:entry-point')
   static Future<String> getOS() async {
     final AndroidDeviceInfo androidDeviceInfo =
-        await DeviceInfoPlugin().androidInfo;
+    await DeviceInfoPlugin().androidInfo;
     return 'Android ${androidDeviceInfo.version.release}';
   }
 
   @pragma('vm:entry-point')
   static Future<String> getModel() async {
     final AndroidDeviceInfo androidDeviceInfo =
-        await DeviceInfoPlugin().androidInfo;
+    await DeviceInfoPlugin().androidInfo;
     return '${androidDeviceInfo.manufacturer} ${androidDeviceInfo.model}';
   }
 
@@ -39,5 +43,30 @@ class DeviceMapper {
         'model': await getModel(),
       }),
     );
+  }
+}
+
+class ReviewMapper {
+  Future<bool> canShowReview() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String reviewType = prefs.getString('reviewType') ?? 'pending';
+
+    if (reviewType == 'never' || reviewType == 'accepted') {
+      return false;
+    }
+
+    final int reviewCount = ((prefs.getInt('reviewCount') ?? 0) + 1) % 5;
+    await prefs.setInt('reviewCount', reviewCount);
+    return reviewCount == 0 && (await InAppReview.instance.isAvailable());
+  }
+
+  Future<void> acceptReview() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('reviewType', 'accepted');
+  }
+
+  Future<void> neverReview() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('reviewType', 'never');
   }
 }
