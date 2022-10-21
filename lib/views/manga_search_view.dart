70936d1d2c8dd6e906d80c72f8e06d766c23387c
Ziedelth <watteaupierre@gmail.com>
2022-10-21 18:53:12 +0200

    fix

--- lib/views/manga_search_view.dart
+++ lib/views/manga_search_view.dart
@@ -13,31 +13,50 @@ class MangaSearchView extends StatefulWidget {
 
 class _MangaSearchViewState extends State<MangaSearchView> {
   final MangaMapper _mangaMapper = MangaMapper();
+  bool _isShowned = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MobileScanner(
-        allowDuplicates: false,
-        onDetect: (Barcode barcode, _) async {
-          if (barcode.rawValue == null) {
-            return;
-          }
+      body: Stack(
+        children: <Widget>[
+          MobileScanner(
+            allowDuplicates: false,
+            onDetect: (Barcode barcode, _) async {
+              if (barcode.rawValue == null || _isShowned) {
+                return;
+              }
 
-          final String ean = barcode.rawValue!;
-          final Manga? manga = await _mangaMapper.search(ean);
+              final String ean = barcode.rawValue!;
+              final Manga? manga = await _mangaMapper.search(ean);
 
-          if (manga == null) {
-            return;
-          }
+              if (manga == null) {
+                return;
+              }
 
-          showBottomSheet(
-            context: context,
-            builder: (_) {
-              return MangaWidget(manga: manga);
+              _isShowned = true;
+              await showModalBottomSheet(
+                context: context,
+                builder: (_) {
+                  return MangaWidget(manga: manga);
+                },
+              );
+              _isShowned = false;
             },
-          );
-        },
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: IconButton(
+              onPressed: () async {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
