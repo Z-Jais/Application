9d044e5b64f0f5867c3784cecdba170d3211536c
Ziedelth <watteaupierre@gmail.com>
2022-10-26 22:44:26 +0200

    fix

--- lib/mappers/notifications_mapper.dart
+++ lib/mappers/notifications_mapper.dart
@@ -1,35 +1,65 @@
 import 'dart:convert';
 
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 @pragma('vm:entry-point')
-Future<void> _onBackgroundAlarm() async {
-  debugPrint('Background alarm');
+Future<void> _show({
+  required int id,
+  required String channelId,
+  required String channelName,
+  String? image,
+  String? title,
+  String? body,
+  bool requestPermission = false,
+}) async {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  AndroidInitializationSettings initializationSettingsAndroid =
+      AndroidInitializationSettings(image ?? 'ic_stat_notifications');
 
+  await flutterLocalNotificationsPlugin.initialize(
+    InitializationSettings(android: initializationSettingsAndroid),
+  );
+
+  if (requestPermission) {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>()
+        ?.requestPermission();
+  }
+
+  await flutterLocalNotificationsPlugin.show(
+    id,
+    title,
+    body,
+    NotificationDetails(
+      android: AndroidNotificationDetails(
+        channelId,
+        channelName,
+      ),
+    ),
+  );
+}
+
+@pragma('vm:entry-point')
+Future<void> _onBackgroundAlarm() async {
   final NotificationsMapper notificationsMapper = NotificationsMapper();
+  final List<String> checkedUuids = await notificationsMapper.getCheckedUuids();
   final String lastCheck = await notificationsMapper.getLastCheck();
-  debugPrint('Connecting...');
+
   final WebSocketChannel channel = WebSocketChannel.connect(
     Uri.parse('wss://beta-api.ziedelth.fr/notifications'),
   );
-  debugPrint('Connected');
-  debugPrint('Sending last check...');
   channel.sink.add('fr;$lastCheck');
-  debugPrint('Sent');
-  debugPrint('Waiting for response...');
   final String response = await channel.stream.first;
-  debugPrint('Received');
-  debugPrint('Response: $response');
-  debugPrint('Closing...');
   await channel.sink.close();
 
   final List<Map<String, dynamic>> data =
       List<Map<String, dynamic>>.from(jsonDecode(response));
-  final List<String> checkedUuids = await notificationsMapper.getCheckedUuids();
+
   final List<String> uuids = data
       .map((Map<String, dynamic> element) => element['uuid'] as String)
       .toList();
@@ -42,23 +72,18 @@ Future<void> _onBackgroundAlarm() async {
       .join(', ');
 
   if (animes.isNotEmpty) {
-    debugPrint('Sending notification...');
-
-    await notificationsMapper.show(
+    await _show(
       id: 0,
       channelId: 'notifications',
       channelName: 'Notifications',
       body: animes,
     );
-
-    debugPrint('Sent');
   }
 
   await notificationsMapper.setLastCheck();
   await notificationsMapper.setCheckedUuids(uuids);
 }
 
-@pragma('vm:entry-point')
 class NotificationsMapper {
   final String _lastCheckKey = 'lastCheck';
 
@@ -100,7 +125,7 @@ class NotificationsMapper {
     }
 
     final bool set = await AndroidAlarmManager.periodic(
-      const Duration(minutes: 5),
+      const Duration(minutes: 1),
       0,
       _onBackgroundAlarm,
       allowWhileIdle: true,
@@ -110,44 +135,5 @@ class NotificationsMapper {
     );
 
     return set;
-  }
-
-  Future<void> show({
-    required int id,
-    required String channelId,
-    required String channelName,
-    String? image,
-    String? title,
-    String? body,
-    bool requestPermission = false,
-  }) async {
-    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-    AndroidInitializationSettings initializationSettingsAndroid =
-        AndroidInitializationSettings(image ?? 'ic_stat_notifications');
-
-    await flutterLocalNotificationsPlugin.initialize(
-      InitializationSettings(android: initializationSettingsAndroid),
-    );
-
-    if (requestPermission) {
-      await flutterLocalNotificationsPlugin
-          .resolvePlatformSpecificImplementation<
-              AndroidFlutterLocalNotificationsPlugin>()
-          ?.requestPermission();
-    }
-
-    await flutterLocalNotificationsPlugin.show(
-      id,
-      title,
-      body,
-      NotificationDetails(
-        android: AndroidNotificationDetails(
-          channelId,
-          channelName,
-          importance: Importance.max,
-        ),
-      ),
-    );
   }
 }
