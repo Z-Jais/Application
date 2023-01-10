3f37536ee783bf2f7476342e92602e6cf1c3d90c
Ziedelth <watteaupierre@gmail.com>
2023-01-10 16:40:49 +0100

    Add system tray

---
+++ lib/controllers/platforms/windows_platform_controller.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:system_tray/system_tray.dart';
+import 'package:window_manager/window_manager.dart';
+
+class WindowsPlatformController {
+  Future<void> init() async {
+    await windowManager.ensureInitialized();
+
+    const WindowOptions windowOptions = WindowOptions(
+      minimumSize: Size(800, 600),
+      center: true,
+      title: 'Jaïs',
+    );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+    });
+
+    final SystemTray systemTray = SystemTray();
+    await systemTray.initSystemTray(
+      iconPath: 'assets/icon.ico',
+      title: 'Jaïs',
+      toolTip: 'Jaïs',
+    );
+
+    final Menu menu = Menu();
+    await menu.buildFrom([
+      MenuItemLabel(
+        label: 'Afficher',
+        onClicked: (menuItem) => windowManager.show(),
+      ),
+      MenuItemLabel(
+        label: 'Cacher',
+        onClicked: (menuItem) => windowManager.hide(),
+      ),
+      MenuSeparator(),
+      MenuItemLabel(
+        label: 'Quitter',
+        onClicked: (menuItem) => windowManager.close(),
+      ),
+    ]);
+
+    await systemTray.setContextMenu(menu);
+
+    systemTray.registerSystemTrayEventHandler((eventName) {
+      if (eventName == kSystemTrayEventClick) {
+        Platform.isWindows
+            ? windowManager.show()
+            : systemTray.popUpContextMenu();
+      } else if (eventName == kSystemTrayEventRightClick) {
+        Platform.isWindows
+            ? systemTray.popUpContextMenu()
+            : windowManager.show();
+      }
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,49 @@ import 'package:jais/controllers/animes/anime_detail_controller.dart';
 import 'package:jais/controllers/animes/anime_diary_controller.dart';
 import 'package:jais/controllers/animes/anime_search_controller.dart';
 import 'package:jais/controllers/app_controller.dart';
+import 'package:jais/controllers/platforms/windows_platform_controller.dart';
 import 'package:jais/views/animes/anime_detail_view.dart';
 import 'package:jais/views/animes/anime_diary_view.dart';
 import 'package:jais/views/animes/anime_search_view.dart';
 import 'package:jais/views/initialization_view.dart';
 import 'package:provider/provider.dart';
-import 'package:window_size/window_size.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isWindows) {
-    setWindowTitle('Jaïs');
-    setWindowMinSize(const Size(800, 600));
+    await WindowsPlatformController().init();
   }
 
   runApp(
     ChangeNotifierProvider(
       create: (_) => AppController(),
-      child: const MyApp(),
+      child: MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget with WindowListener {
+  MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WindowListener {
   static const Color _mainColor = Color(0xFFF2B05E);
 
-  const MyApp({super.key});
+  @override
+  void initState() {
+    log('_MyAppState.initState()');
+    windowManager.addListener(this);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    log('MyApp.build()');
+    log('_MyAppState.build()');
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -77,5 +89,17 @@ class MyApp extends StatelessWidget {
         },
       },
     );
+  }
+
+  @override
+  void onWindowMinimize() {
+    windowManager.hide();
+    super.onWindowMinimize();
+  }
+
+  @override
+  void onWindowRestore() {
+    setState(() { });
+    super.onWindowRestore();
   }
 }
