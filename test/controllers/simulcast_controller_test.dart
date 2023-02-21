136c11272cd0ad2e0a6ae6b431a60d7934ca9b43
Ziedelth <watteaupierre@gmail.com>
2023-02-21 12:37:59 +0100

    Add Simulcast test

---
+++ test/controllers/simulcast_controller_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:jais/controllers/simulcast_controller.dart';
+
+void main() {
+  group('SimulcastController', () {
+    test('Init', () async {
+      final controller = SimulcastController();
+      await Future.delayed(const Duration(seconds: 1));
+
+      expect(controller.list, isNotEmpty);
+      expect(controller.list, contains(controller.loadingWidget));
+      expect(controller.lastPageError, isFalse);
+      expect(controller.nothingToShow(), isFalse);
+    });
+    
+    test('Load', () async {
+      final controller = SimulcastController();
+      await controller.load();
+      await Future.delayed(const Duration(seconds: 1));
+
+      expect(controller.list, isNotEmpty);
+      expect(controller.list, isNot(contains(controller.loadingWidget)));
+      expect(controller.lastPageError, isFalse);
+      expect(controller.nothingToShow(), isFalse);
+    });
+  });
+}
