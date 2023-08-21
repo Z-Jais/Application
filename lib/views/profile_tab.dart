1b149441daa6b305318ca2ee99c844d225be4e30
Ziedelth <watteaupierre@gmail.com>
2023-08-21 23:57:04 +0200

    Added ScrollView and TextOverflow in profile_tab.dart and category_button.dart

--- lib/views/profile_tab.dart
+++ lib/views/profile_tab.dart
@@ -39,176 +39,181 @@ class ProfileTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        children: [
-          ChangeNotifierProvider.value(
-            value: NotificationController.instance,
-            child: Consumer<NotificationController>(
-              builder: (_, value, __) {
-                return Category(
-                  label: 'NOTIFICATION',
-                  buttons: [
-                    FutureBuilder(
-                      future: value.isAll,
-                      builder: (context, snapshot) {
-                        final isEnabled = snapshot.data == true;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            ChangeNotifierProvider.value(
+              value: NotificationController.instance,
+              child: Consumer<NotificationController>(
+                builder: (_, value, __) {
+                  return Category(
+                    label: 'NOTIFICATION',
+                    buttons: [
+                      FutureBuilder(
+                        future: value.isAll,
+                        builder: (context, snapshot) {
+                          final isEnabled = snapshot.data == true;
 
-                        return CategoryButton(
-                          label: 'Toutes',
-                          icon: Icon(
-                            isEnabled
-                                ? Icons.notifications_active
-                                : Icons.notifications_off_outlined,
-                          ),
-                          trailing: isEnabled ? const Icon(Icons.check) : null,
-                          onTap: () async {
-                            if (!await _checkIfSomethingIsRunning(
-                              value,
-                              context,
-                            )) {
-                              await value
-                                  .setNotificationType(NotificationType.all);
-                            }
-                          },
-                        );
-                      },
-                    ),
-                    FutureBuilder(
-                      future: value.isWatchlist,
-                      builder: (context, snapshot) {
-                        final isEnabled = snapshot.data == true;
-
-                        return CategoryButton(
-                          label: 'Watchlist',
-                          icon: Icon(
-                            isEnabled
-                                ? Icons.edit_notifications
-                                : Icons.edit_notifications_outlined,
-                          ),
-                          trailing: isEnabled ? const Icon(Icons.check) : null,
-                          onTap: () async {
-                            if (!await _checkIfSomethingIsRunning(
-                              value,
-                              context,
-                            )) {
-                              await value.setNotificationType(
-                                NotificationType.watchlist,
-                              );
-                            }
-                          },
-                        );
-                      },
-                    ),
-                    FutureBuilder(
-                      future: value.isDisabled,
-                      builder: (context, snapshot) {
-                        final isEnabled = snapshot.data == true;
-
-                        return CategoryButton(
-                          label: 'Désactivé',
-                          icon: Icon(
-                            isEnabled
-                                ? Icons.notifications_off
-                                : Icons.notifications_off_outlined,
-                          ),
-                          trailing: isEnabled ? const Icon(Icons.check) : null,
-                          onTap: () async {
-                            if (!await _checkIfSomethingIsRunning(
-                              value,
-                              context,
-                            )) {
-                              await value.setNotificationType(
-                                NotificationType.disabled,
-                              );
-                            }
-                          },
-                        );
-                      },
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
-          const SizedBox(height: 32),
-          Category(
-            label: 'ASSISTANCE',
-            buttons: [
-              CategoryButton(
-                label: 'Nous contacter',
-                icon: const Icon(Icons.email_outlined),
-                onTap: () {
-                  URLController().mailTo('jais@ziedelth.fr');
-                },
-              ),
-              CategoryButton(
-                label: 'X (Twitter)',
-                icon: SvgPicture.asset(
-                  'assets/x-twitter.svg',
-                  width: 24,
-                  height: 24,
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.onSurface,
-                    BlendMode.srcIn,
-                  ),
-                ),
-                onTap: () {
-                  URLController().goOnUrl('https://x.com/Jaiss___');
-                },
-              ),
-            ],
-          ),
-          const SizedBox(height: 32),
-          Category(
-            label: 'AUTRE',
-            buttons: [
-              CategoryButton(
-                label: 'Suppression des données',
-                icon: const Icon(Icons.delete_forever_outlined),
-                onTap: () {
-                  final appController =
-                      Provider.of<AppController>(context, listen: false);
-
-                  showDialog(
-                    context: context,
-                    builder: (_) => AlertDialog(
-                      title: const Text('Suppression des données'),
-                      content: const Text(
-                        'Êtes-vous sûr de vouloir supprimer toutes vos données ?',
+                          return CategoryButton(
+                            label: 'Toutes',
+                            icon: Icon(
+                              isEnabled
+                                  ? Icons.notifications_active
+                                  : Icons.notifications_off_outlined,
+                            ),
+                            trailing:
+                                isEnabled ? const Icon(Icons.check) : null,
+                            onTap: () async {
+                              if (!await _checkIfSomethingIsRunning(
+                                value,
+                                context,
+                              )) {
+                                await value
+                                    .setNotificationType(NotificationType.all);
+                              }
+                            },
+                          );
+                        },
                       ),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: const Text('Annuler'),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                            appController.reset();
-                          },
-                          child: const Text('Supprimer'),
-                        ),
-                      ],
+                      FutureBuilder(
+                        future: value.isWatchlist,
+                        builder: (context, snapshot) {
+                          final isEnabled = snapshot.data == true;
+
+                          return CategoryButton(
+                            label: 'Watchlist',
+                            icon: Icon(
+                              isEnabled
+                                  ? Icons.edit_notifications
+                                  : Icons.edit_notifications_outlined,
+                            ),
+                            trailing:
+                                isEnabled ? const Icon(Icons.check) : null,
+                            onTap: () async {
+                              if (!await _checkIfSomethingIsRunning(
+                                value,
+                                context,
+                              )) {
+                                await value.setNotificationType(
+                                  NotificationType.watchlist,
+                                );
+                              }
+                            },
+                          );
+                        },
+                      ),
+                      FutureBuilder(
+                        future: value.isDisabled,
+                        builder: (context, snapshot) {
+                          final isEnabled = snapshot.data == true;
+
+                          return CategoryButton(
+                            label: 'Désactivé',
+                            icon: Icon(
+                              isEnabled
+                                  ? Icons.notifications_off
+                                  : Icons.notifications_off_outlined,
+                            ),
+                            trailing:
+                                isEnabled ? const Icon(Icons.check) : null,
+                            onTap: () async {
+                              if (!await _checkIfSomethingIsRunning(
+                                value,
+                                context,
+                              )) {
+                                await value.setNotificationType(
+                                  NotificationType.disabled,
+                                );
+                              }
+                            },
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 32),
+            Category(
+              label: 'ASSISTANCE',
+              buttons: [
+                CategoryButton(
+                  label: 'Nous contacter',
+                  icon: const Icon(Icons.email_outlined),
+                  onTap: () {
+                    URLController().mailTo('jais@ziedelth.fr');
+                  },
+                ),
+                CategoryButton(
+                  label: 'X (Twitter)',
+                  icon: SvgPicture.asset(
+                    'assets/x-twitter.svg',
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.onSurface,
+                      BlendMode.srcIn,
                     ),
-                  );
-                },
-              ),
-              CategoryButton(
-                label: 'Demande de fonctionnalité / Signaler un bug',
-                icon: const Icon(Icons.add_box_outlined),
-                onTap: () {
-                  URLController().goOnUrl(
-                    'https://github.com/Z-Jais/Application/issues/new',
-                  );
-                },
-              ),
-            ],
-          ),
-        ],
+                  ),
+                  onTap: () {
+                    URLController().goOnUrl('https://x.com/Jaiss___');
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 32),
+            Category(
+              label: 'AUTRE',
+              buttons: [
+                CategoryButton(
+                  label: 'Suppression des données',
+                  icon: const Icon(Icons.delete_forever_outlined),
+                  onTap: () {
+                    final appController =
+                        Provider.of<AppController>(context, listen: false);
+
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: const Text('Suppression des données'),
+                        content: const Text(
+                          'Êtes-vous sûr de vouloir supprimer toutes vos données ?',
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('Annuler'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                              appController.reset();
+                            },
+                            child: const Text('Supprimer'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                CategoryButton(
+                  label: 'Demande de fonctionnalité / Signaler un bug',
+                  icon: const Icon(Icons.add_box_outlined),
+                  onTap: () {
+                    URLController().goOnUrl(
+                      'https://github.com/Z-Jais/Application/issues/new',
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
